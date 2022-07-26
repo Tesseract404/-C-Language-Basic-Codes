ce99c421e0a423234d2b8983f75d53cec757113a
Tesseract404 <106398932+Tesseract404@users.noreply.github.com>
2022-07-26 23:00:41 +0600

    Delete C Files directory

--- C Files/count.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a = 8, b = 2, c = 4;
-    int x;
-    x=((a+b)/c)*((a−c)/b ) ;
-    printf("%d",x);
-}
--- C Files/try.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-double x=10.5,y=7.2;
-int m=5,n=2;
-double k=10.5/(double)5;
-printf("%lf",k);
-}
